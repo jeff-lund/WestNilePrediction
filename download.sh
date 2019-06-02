74c2b94d000908332bcdda321a8442c3ae6b5ba0
Colleen Rooney <crooney@pdx.edu>
2019-06-02 09:18:05 -0700

    prelim download script for google drive

--- download.sh
+++ download.sh
@@ -17,15 +17,20 @@ google_drive_download() {
 	rm cookies.txt confirm.txt
      }
 
+# Change this variable to download data to a different directory
 DIR=input
 
+# Exit script if directory already exists
 if [ -d "$DIR" ]; then
 	echo "Directory $DIR exists. Move this directory to download data from Google Drive"
 	exit 1
 fi
 
+# Download the files from Jeff's Google Drive
 mkdir $DIR
 cd $DIR
 google_drive_download 1YrRTBPMeC3L2ilL71GGj37EAXkAbFT1d predict-west-nile-virus.zip
 google_drive_download 1BL-Rpz4AU7g14-6-jWc9sQt1Td3yvBQ chicago-west-nile-virus-mosquito-test-results.zip
 google_drive_download 1LpPStAaL6818teaGXCZWhGMSdwADhRjC NOAA-weather-data.csv
+
+# Unzip files
