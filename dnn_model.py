8db67b4f37baf651810e231e8bb9ab9a0869dfc3
jeff-lund <jdl3@pdx.edu>
2019-06-06 19:37:04 -0700

    altering deep NN model

--- dnn_model.py
+++ dnn_model.py
@@ -1,13 +1,14 @@
 from keras.layers import Dense, Activation, Dropout
 from keras.models import Sequential
+from keras import regularizers
 import keras
 
 
 def create_model(input_dim, p=0.5, activation='sigmoid'):
     model = Sequential()
-    model.add(Dense(10, input_dim=input_dim))
+    model.add(Dense(100, kernel_regularizer=regularizers.l2(0.01), input_dim=input_dim))
     model.add(Dropout(p))
-    model.add(Dense(10))
+    model.add(Dense(100, kernel_regularizer=regularizers.l2(0.01)))
     model.add(Dropout(p))
     model.add(Dense(1))
     model.add(Activation(activation))
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import dataset
 
 x_train, y_train, x_test = dataset.dataset()
 model = create_model(x_train[0].shape[0], p=0.6)
-model.compile(optimizer=Adam(lr=0.0001),
-              loss='mse',
-              metrics=['mse'])
-model.fit(x_train, y_train, epochs=50, batch_size=4)
+optimizer = Adam(lr=0.001)
+model.compile(optimizer=optimizer,
+              loss='binary_crossentropy',
+              metrics=['mse', 'accuracy'])
+model.fit(x_train, y_train, epochs=6000, batch_size=5000)
 print(model.evaluate(x_train, y_train))
 score = model.predict_on_batch(x_train[:32])
 print(score)
